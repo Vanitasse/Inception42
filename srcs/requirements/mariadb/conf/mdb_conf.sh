02b904829ba9a48ca9181d2c60a34e50694bb03f
vanitas <mblatier@outlook.fr>
2024-08-19 14:13:01 +0200

    inception

---
+++ srcs/requirements/mariadb/conf/mdb_conf.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#---------------------------------------------------mariadb start---------------------------------------------------#
+
+service mariadb start
+sleep 5
+
+#---------------------------------------------------mariadb config---------------------------------------------------#
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
+
+#---------------------------------------------------mariadb restart---------------------------------------------------#
+
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
