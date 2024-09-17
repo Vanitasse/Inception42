6840da547c633186e18fb30df316ae2879845848
mablatie <mablatie@mablatie.mablatie.42.fr>
2024-09-17 17:30:01 +0200

    FINAL

--- srcs/requirements/mariadb/conf/mdb_conf.sh
+++ srcs/requirements/mariadb/conf/mdb_conf.sh
@@ -6,12 +6,14 @@ service mariadb start
 sleep 5
 
 #---------------------------------------------------mariadb config---------------------------------------------------#
-#mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
+
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 #---------------------------------------------------mariadb restart---------------------------------------------------#
 
